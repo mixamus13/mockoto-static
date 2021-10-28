a9ceab9ea8ee78f3ac658d014ba39ac8b24ac411
Max Dokuchaev <mixamus@icloud.com>
2021-10-28 10:24:39 +0300

    Add test Mockito static

--- src/main/java/com/example/mockotostatic/StaticUtils.java
+++ src/main/java/com/example/mockotostatic/StaticUtils.java
@@ -1,13 +1,13 @@
 package com.example.mockotostatic;
 
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@NoArgsConstructor
 public class StaticUtils {
-
-    private StaticUtils() {
-    }
 
     public static List<Integer> range(int start, int end) {
         return IntStream.range(start, end)
